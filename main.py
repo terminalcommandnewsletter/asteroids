e3575039bc7b28c043dee2fcc5c3fdcacb662178
terminalcommandnewsletter <terminalcommandnewsletter@gmail.com>
2024-08-29 22:54:10 +0530

    add player rotate code

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def main():
         pygame.display.flip()
         tick = clock.tick(60)
         dt = tick/1000
+        player.update(dt)
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,10 +1,10 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+import constants
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        super().__init__(x,y,PLAYER_RADIUS)
+        super().__init__(x,y,constants.PLAYER_RADIUS)
         self.position = pygame.Vector2(x,y)
         self.rotation = 0
     
@@ -19,3 +19,14 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.rotation += constants.PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
