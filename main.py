cc9c9c0a3ab06d74a1c40f3d7f18d233446f694e
terminalcommandnewsletter <terminalcommandnewsletter@gmail.com>
2024-08-29 23:00:07 +0530

    use groups

--- main.py
+++ main.py
@@ -13,17 +13,22 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        for element in drawable:
+            element.draw(screen)
         pygame.display.flip()
         tick = clock.tick(60)
         dt = tick/1000
-        player.update(dt)
+        for element in updatable:
+            element.update(dt)
 
 if __name__ == "__main__":
     main()
